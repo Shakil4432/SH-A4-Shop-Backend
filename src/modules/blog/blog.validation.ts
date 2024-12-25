f733dc9a3c6f02bd2b9a65f5f1fed11021374618
Shakil4432 <shakil4432@gmail.com>
2024-12-25 19:51:50 +0600

    vercel deploy again

--- src/modules/blog/blog.validation.ts
+++ src/modules/blog/blog.validation.ts
@@ -4,7 +4,7 @@ const blogValidationSchema = z.object({
   body: z.object({
     title: z.string({ required_error: 'title is required' }),
     content: z.string({ required_error: 'content is required' }),
-
+    author: z.string().optional(),
     isPublished: z.boolean().default(true),
   }),
 });
