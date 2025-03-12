85b29018043c87324247def702284f5b34c65e40
Shakil4432 <shakil4432@gmail.com>
2025-03-13 03:06:36 +0600

    deploy

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,12 @@ const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(
+  cors({
+    origin: 'https://stationary-shop-frontend-six.vercel.app/',
+    credentials: true,
+  }),
+);
 
 // application routes
 app.use('/api/users', UserRoutes);
